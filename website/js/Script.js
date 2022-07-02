ce2c7da90f2eb3e2e7bb107bbcb71737a3fa971d
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2022-07-02 19:45:25 +0200

    Structure of Metadata and Analysis sections

--- website/js/Script.js
+++ website/js/Script.js
@@ -8,6 +8,33 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
+
+    $("#Legal").click(function(){
+        $("#Legal").addClass("is-active");
+        $("#Original").removeClass("is-active");
+        $("#Final").removeClass("is-active");
+        $(".legal").removeClass("is-hidden");
+        $(".original").addClass("is-hidden");
+        $(".final").addClass("is-hidden");
+    });
+
+    $("#Original").click(function(){
+        $("#Legal").removeClass("is-active");
+        $("#Original").addClass("is-active");
+        $("#Final").removeClass("is-active");
+        $(".legal").addClass("is-hidden");
+        $(".original").removeClass("is-hidden");
+        $(".final").addClass("is-hidden");
+    });
+
+    $("#Final").click(function(){
+        $("#Legal").removeClass("is-active");
+        $("#Original").removeClass("is-active");
+        $("#Final").addClass("is-active");
+        $(".legal").addClass("is-hidden");
+        $(".original").addClass("is-hidden");
+        $(".final").removeClass("is-hidden");
+    });
+
   });
 
-  

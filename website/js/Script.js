a33ccab226e8e135e309436a82a866b9a783e864
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2022-07-02 20:39:25 +0200

    chart library

--- website/js/Script.js
+++ website/js/Script.js
@@ -38,3 +38,28 @@ $(document).ready(function() {
 
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});

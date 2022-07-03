22a404ebfa329462312af269fa4fe1c82e6df1ed
ChiaraCati <chiara.catizone@studio.unibo.it>
2022-07-03 11:19:39 +0200

    togli query minerva script

--- software/query_minerva.py
+++
@@ -1,6 +0,0 @@
-import urllib
-from urllib.request import urlopen
-
-url = 'https://datacatalog.regione.emilia-romagna.it/catalogCTA/api/action/datastore_search?resource_id=1f56dc65-bc60-4e40-875e-d2c56318b034&limit=5&q=title:jones'
-fileobj = urlopen(url)
-print(fileobj.read())

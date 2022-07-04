47e6e465430e6cc4429014c639c3cf5063a4486e
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2022-07-04 18:16:47 +0200

    map

--- website/js/Script.js
+++ website/js/Script.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
 
   });
 
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const chart = Highcharts.chart('container', {
         chart: {
             type: 'bar'
@@ -62,4 +62,88 @@ $(document).ready(function() {
             data: [5, 7, 3]
         }]
     });
+
+(async () => {
+
+    const topology = await fetch(
+        'website/data/emilia.json'
+    ).then(response => response.json());
+
+    const data = [];
+    
+   Highcharts.getJSON('website/data/pop.json', function (data) {
+    
+        // Make codes uppercase to match the map data
+        data.forEach(function (p) {
+            p.code = p.code.toUpperCase();
+        });
+    
+        // Instantiate the map
+        Highcharts.mapChart('container2', {
+    
+            chart: {
+                map: topology,
+                borderWidth: 1
+            },
+    
+            title: {
+                text: 'Emilia Romagna population density (/km²)'
+            },
+    
+            exporting: {
+                sourceWidth: 600,
+                sourceHeight: 500
+            },
+    
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: 'rgba(255,255,255,0.85)',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+    
+            mapNavigation: {
+                enabled: true
+            },
+    
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#EEEEFF',
+                maxColor: '#035f9c',
+                stops: [
+                    [0, '#EFEFFF'],
+                    [0.67, '#85ceff'],
+                    [1, '#035f9c']
+                ]
+            },
+    
+            series: [{
+                accessibility: {
+                    point: {
+                        valueDescriptionFormat: '{xDescription}, {point.value} people per square kilometer.'
+                    }
+                },
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Population density',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}/km²'
+                }
+            }]
+        });
+    });
+    
+    })();
+
 });

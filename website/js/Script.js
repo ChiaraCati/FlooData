5f13a931152dc8563ec5323aef903db7724bc628
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2022-08-24 22:31:51 +0200

    up

--- website/js/Script.js
+++ website/js/Script.js
@@ -61,11 +61,10 @@ document.addEventListener('DOMContentLoaded', function () {
     
 (async () => {
 
-    const mapData = await fetch(
+    const topology = await fetch(
         'website/data/emilia.json'
     ).then(response => response.json());
 
-    
     Highcharts.getJSON('website/data/MD2.json', function (data) {
     
       data.forEach(function (p) {
@@ -77,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
       
       Highcharts.mapChart('disaster_map', {
         chart: {
-          map: mapData
+          map: topology
         },
         title: {
           text: 'Water disaster in Emilia Romagna'
@@ -597,7 +596,10 @@ document.addEventListener('DOMContentLoaded', function () {
         
                 // Instantiate the map
                 Highcharts.mapChart('risk_distribution_map', {
-        
+
+                    chart: {
+                      map: topology,
+                    },
                     exporting: {
                         sourceWidth: 600,
                         sourceHeight: 500
@@ -628,7 +630,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     },
         
                     series: [{
-                        mapData,
                         accessibility: {
                             point: {
                                 valueDescriptionFormat: '{xDescription}, {point.high_hydraulic_risk_area_p3} people per square kilometer.'
@@ -655,6 +656,9 @@ document.addEventListener('DOMContentLoaded', function () {
             
         }
     } 
+
+
+
 
     Highcharts.chart("finance_bar", {
 
@@ -1222,6 +1226,222 @@ document.addEventListener('DOMContentLoaded', function () {
             pointFormat: "<b>Financings for {point.name}:</b> {point.value} €"
         }
       });
+
+
+
+
+
+      const csv = await fetch(
+        'website/data/line_series.csv'
+    ).then(response => response.text());
+  
+    // Very simple and case-specific CSV string splitting
+    const CSVtoArray = text => text.split(',');
+  
+    const csvArr = csv.split(/\n/),
+        province = {},
+        numRegex = /^[0-9\.]+$/,
+        lastCommaRegex = /,\s$/,
+        quoteRegex = /['"\s]+/g;
+        categories = CSVtoArray(csvArr[0]).slice(2);
+  
+    let provChart;
+  
+    // Parse the CSV into arrays, one array each prov
+    csvArr.slice(1).forEach(function (line) {
+        var row = CSVtoArray(line),
+            data = row.slice(2);
+        
+        
+  
+        data.forEach(function (val, i) {
+            val = val.replace(quoteRegex, '');
+            if (numRegex.test(val)) {
+                val = parseInt(val, 10);
+            } else if (!val || lastCommaRegex.test(val)) {
+                val = null;
+            }
+            data[i] = val;
+        });
+  
+        var code = row[1].replace(/\s+/g, '')
+  
+        province[code] = {
+            name: row[0],
+            code3: code,
+            data: data
+        };
+  
+        
+    });
+    
+  
+    // For each prov, use the latest value for current population
+    const data = [];
+    for (const code3 in province) {
+        if (Object.hasOwnProperty.call(province, code3)) {
+            const itemData = province[code3].data;
+            let value = null,
+                i = itemData.length,
+                year;
+  
+            while (i--) {
+                if (typeof itemData[i] === 'number') {
+                    value = itemData[i];
+                    year = categories[i];
+                    break;
+                }
+            }
+            data.push({
+                name: province[code3].name,
+                code3: code3,
+                value: value,
+                year: year.replace(/\s+/g, '')
+            });
+        }
+    }
+  
+  
+    // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
+    const mapData = Highcharts.geojson(topology);
+    mapData.forEach(function (prov) {
+        prov.id = prov.properties['hc-key']; // for Chart.get()
+    });
+  
+    
+  
+    // Wrap point.select to get to the total selected points
+    Highcharts.wrap(Highcharts.Point.prototype, 'select', function (proceed) {
+  
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+  
+        const points = mapChart.getSelectedPoints();
+        if (points.length) {
+            if (points.length === 1) {
+                document.querySelector('#info h2').innerHTML = points[0].name;
+            } else {
+                document.querySelector('#info h2').innerHTML = 'Comparing province';
+  
+            }
+            document.querySelector('#info .subheader')
+                .innerHTML = '<h4>Historical population</h4><small><em>Shift + Click on map to compare province</em></small>';
+  
+            if (!provChart) {
+                provChart = Highcharts.chart('province_line_chart', {
+                    chart: {
+                        height: 250,
+                        spacingLeft: 0
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    title: {
+                        text: null
+                    },
+                    subtitle: {
+                        text: null
+                    },
+                    xAxis: {
+                        tickPixelInterval: 50,
+                        crosshair: true
+                    },
+                    yAxis: {
+                        title: null,
+                        opposite: true
+                    },
+                    tooltip: {
+                        split: true
+                    },
+                    plotOptions: {
+                        series: {
+                            animation: {
+                                duration: 500
+                            },
+                            marker: {
+                                enabled: false
+                            },
+                            threshold: 0,
+                            pointStart: parseInt(categories[0], 10)
+                        }
+                    }
+                });
+            }
+  
+            provChart.series.slice(0).forEach(function (s) {
+                s.remove(false);
+            });
+            points.forEach(function (p) {
+                provChart.addSeries({
+                    name: p.name,
+                    data: province[p.code3].data,
+                    type: points.length > 1 ? 'line' : 'area'
+                }, false);
+            });
+            provChart.redraw();
+  
+        } else {
+            document.querySelector('#info h2').innerHTML = '';
+            document.querySelector('#info .subheader').innerHTML = '';
+            if (provChart) {
+                provChart = provChart.destroy();
+            }
+        }
+    });
+  
+    // Initiate the map chart
+    const mapChart = Highcharts.mapChart('finance_map', {
+  
+        chart: {
+            map: topology
+        },
+  
+        title: {
+            text: 'Population history by prov'
+        },
+  
+        subtitle: {
+            text: 'Source: <a href="http://data.worldbank.org/indicator/SP.POP.TOTL/province/1W?display=default">The World Bank</a>'
+        },
+  
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+  
+        colorAxis: {
+            type: 'logarithmic',
+            endOnTick: false,
+            startOnTick: false,
+            min: 50000
+        },
+  
+        tooltip: {
+            footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
+        },
+  
+        series: [{
+            data: data,
+            mapData: mapData,
+            joinBy: ['postal-code', 'code3'],
+            name: 'Current population',
+            allowPointSelect: true,
+            cursor: 'pointer',
+            states: {
+                select: {
+                    color: '#a4edba',
+                    borderColor: 'black',
+                    dashStyle: 'shortdot'
+                }
+            },
+            borderWidth: 0.5
+        }]
+    });
+  
+    // Pre-select a prov
+    mapChart.get('it-bo').select();
+  
 
 
     

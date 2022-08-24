c67a57a5b5887fa4ef72f27e63d92bbeb7da5cfe
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2022-08-24 16:22:15 +0200

    up

--- website/js/Script.js
+++ website/js/Script.js
@@ -9,29 +9,29 @@ $(document).ready(function() {
   
     });
 
-    $("#Legal").click(function(){
-        $("#Legal").addClass("is-active");
+    $("#Catalogue").click(function(){
+        $("#Catalogue").addClass("is-active");
         $("#Original").removeClass("is-active");
         $("#Final").removeClass("is-active");
-        $(".legal").removeClass("is-hidden");
+        $(".catalogue").removeClass("is-hidden");
         $(".original").addClass("is-hidden");
         $(".final").addClass("is-hidden");
     });
 
     $("#Original").click(function(){
-        $("#Legal").removeClass("is-active");
+        $("#Catalogue").removeClass("is-active");
         $("#Original").addClass("is-active");
         $("#Final").removeClass("is-active");
-        $(".legal").addClass("is-hidden");
+        $(".catalogue").addClass("is-hidden");
         $(".original").removeClass("is-hidden");
         $(".final").addClass("is-hidden");
     });
 
     $("#Final").click(function(){
-        $("#Legal").removeClass("is-active");
+        $("#Catalogue").removeClass("is-active");
         $("#Original").removeClass("is-active");
         $("#Final").addClass("is-active");
-        $(".legal").addClass("is-hidden");
+        $(".catalogue").addClass("is-hidden");
         $(".original").addClass("is-hidden");
         $(".final").removeClass("is-hidden");
     });
